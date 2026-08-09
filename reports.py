b3b2a76c718b1a00d004faa661cd759c8cc1041b
DIVINE CHUKWUDI <dc.nexumco@gmail.com>
2026-08-09 16:36:17 +0100

    updated reports

--- reports.py
+++ reports.py
@@ -5,11 +5,16 @@ def total_sales():
 
     print(f"Total Items Sold: {total}")
 
+    with open("generated_reports/total_sales.txt", "w", encoding="utf-8") as file:
+        file.write(f"Total Items Sold: {total}")
+
 def total_revenue():
     total = database.get_total_revenue()
 
     print(f"Total Revenue: ₦{total}")
 
+    with open("generated_reports/total_revenue.txt", "w", encoding="utf-8") as file:
+        file.write(f"Total Revenue: ₦{total}")
 
 def low_stock():
     low_stock_products = database.get_low_stock()
@@ -21,6 +26,14 @@ def low_stock():
         for product in low_stock_products:
             print(f" - {product[1]}: {product[5]} units")
 
+    with open("generated_reports/low_stock.txt", "w", encoding="utf-8") as file:
+        if not low_stock_products:
+            file.write("No low stock products.")
+        else:
+            file.write("Low Stock Products:\n")
+            for product in low_stock_products:
+                file.write(f" - {product[1]}: {product[5]} units\n")
+
 def most_valuable_customers():
     valuable_customers = database.get_most_valuable_customers()
 
@@ -30,6 +43,14 @@ def most_valuable_customers():
         print("Most Valuable Customers:")
         for customer in valuable_customers:
             print(f" - {customer[0]}: ₦{customer[1]} total spent")
+
+    with open("generated_reports/most_valuable_customers.txt", "w", encoding="utf-8") as file:
+        if not valuable_customers:
+            file.write("No customers found.")
+        else:
+            file.write("Most Valuable Customers:\n")
+            for customer in valuable_customers:
+                file.write(f" - {customer[0]}: ₦{customer[1]} total spent\n")
 
 
 def best_selling_products():
@@ -44,3 +65,13 @@ def best_selling_products():
             print(f"Product ID: {product[0]}")
             print(f"Quantity Sold: {product[1]}")
             print("----------------------------")
+
+    with open("generated_reports/best_selling_products.txt", "w", encoding="utf-8") as file:
+        if not products:
+            file.write("No products found.")
+        else:
+            file.write("===== BEST SELLING PRODUCTS =====\n")
+            for product in products:
+                file.write(f"Product ID: {product[0]}\n")
+                file.write(f"Quantity Sold: {product[1]}\n")
+                file.write("----------------------------\n")
